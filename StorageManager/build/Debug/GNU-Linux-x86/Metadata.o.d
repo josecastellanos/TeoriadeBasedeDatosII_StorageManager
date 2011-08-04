8932d8d7462e7fb268faa8023efc56457cd5f10d
Camilo Rivera <camilo.ripi@gmail.com>
2011-08-04 13:22:47 -0600

    cambios

--- StorageManager/build/Debug/GNU-Linux-x86/Metadata.o.d
+++ StorageManager/build/Debug/GNU-Linux-x86/Metadata.o.d
@@ -1,5 +1,8 @@
-build/Debug/GNU-Linux-x86/Metadata.o: Metadata.cpp Metadata.h Block.h
+build/Debug/GNU-Linux-x86/Metadata.o: Metadata.cpp Metadata.h Block.h \
+ SystemBlock.h
 
 Metadata.h:
 
 Block.h:
+
+SystemBlock.h:
--- StorageManager/build/Debug/GNU-Linux-x86/StorageManager.o.d
+++ StorageManager/build/Debug/GNU-Linux-x86/StorageManager.o.d
@@ -1,5 +1,5 @@
 build/Debug/GNU-Linux-x86/StorageManager.o: StorageManager.cpp \
- StorageManager.h SystemBlock.h Block.h Metadata.h
+ StorageManager.h SystemBlock.h Block.h Metadata.h MetadataContinuo.h
 
 StorageManager.h:
 
@@ -8,3 +8,5 @@ SystemBlock.h:
 Block.h:
 
 Metadata.h:
+
+MetadataContinuo.h:
